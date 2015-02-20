496af740f5cd87a699e5b259b6635830f6945c06
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-02-20 11:53:55 +0100

    Add changing MAC address after finishing each state; add start opts

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+NS = pair
+INTF0 = veth0
+INTF1 = veth1
+IT=5
+
 .PHONY: all compile deps clean dev
 
 all: compile
@@ -15,7 +20,29 @@ dev: compile
 	erl -pa ebin -pa deps/*/ebin \
 	-eval "[application:start(A) || A <- [compiler, syntax_tools, goldrush, lager]], \
 		pr_fsm:start_link(8999, [{state, $(state)}, {ip, $(ip)}, \
-		{peer_ip, $(peer_ip)}, {iterations, 1}])"
+		{peer_ip, $(peer_ip)}, {pair_no, $(pair_no)}, \
+		{intf_name, $(intf)}, {iterations, $(it)}])"
+
+# has to be run with sudo
+test_setup:
+	ip netns add $(NS)
+	ip link add $(INTF0) type veth peer name $(INTF1)
+	ip link set $(INTF1) netns $(NS)
+	ifconfig $(INTF0) 10.0.0.1/24 up
+	ip netns exec $(NS) ifconfig $(INTF1) 10.0.0.2/24 up
+
+# run with sudo
+test_teardown:
+	ip link del $(INTF0)
+	ip netns delete $(NS)
+
+test_pair1:
+	make dev state=active ip="{10,0,0,1}" peer_ip="{10,0,0,2}" \
+	pair_no=1 it=$(IT) intf=$(INTF0)
+
+test_pair2:
+	ip netns exec $(NS) make dev state=passive ip="{10,0,0,2}" \
+	peer_ip="{10,0,0,1}" pair_no=1 it=$(IT) intf=$(INTF1)
 
 
 rebar:
--- src/pr_fsm.erl
+++ src/pr_fsm.erl
@@ -13,9 +13,8 @@
 %% gen_fsm Function Exports
 %% ------------------------------------------------------------------
 
--export([init/1, active/2, passive/2, handle_event/3,
-         handle_sync_event/4, handle_info/3, terminate/3,
-         code_change/4]).
+-export([init/1, active/2, passive/2, terminate/3, handle_event/3,
+         handle_sync_event/4, handle_info/3]).
 
 %% ------------------------------------------------------------------
 %% Types
@@ -24,8 +23,12 @@
 -record(state, {port :: inet:port_number(),
                 ip :: inet:ip_address(),
                 peer_ip :: inet:ip_address(),
+                it :: non_neg_integer(),
                 iterations :: non_neg_integer(),
-                pair_no :: non_neg_integer()}).
+                intf_name :: string(),
+                base_mac :: string(),
+                pair_no :: non_neg_integer(),
+                sock :: inet:socket()}).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -39,37 +42,42 @@ start_link(Port, Opts) ->
 %% ------------------------------------------------------------------
 
 init([Port, Opts]) ->
-    State = #state{port = Port,
-                   ip = proplists:get_value(ip, Opts),
-                   peer_ip = proplists:get_value(peer_ip, Opts),
-                   iterations = proplists:get_value(interations, Opts, 1),
-                   pair_no = proplists:get_value(pair_no, Opts)},
-    {ok, proplists:get_value(state, Opts), State, _Timeout = 0}.
+    PairNo = proplists:get_value(pair_no, Opts),
+    InitState = proplists:get_value(state, Opts),
+    State0 = #state{port = Port,
+                    ip = proplists:get_value(ip, Opts),
+                    peer_ip = proplists:get_value(peer_ip, Opts),
+                    it = 0,
+                    iterations = proplists:get_value(iterations, Opts, 1),
+                    intf_name = proplists:get_value(intf_name, Opts),
+                    base_mac = base_mac(PairNo, InitState),
+                    pair_no = PairNo},
+    State1 = open_socket(State0),
+    {ok, InitState, State1, _Timeout = 0}.
 
-active(timeout, #state{iterations = 0} = State) ->
+active(timeout, #state{it = N} = State) when N > State#state.iterations ->
     lager:info("Finishing in active state"),
     {stop, normal, State};
-active(timeout, #state{iterations = N} = State) ->
-    SockOpts = [{ip, State#state.ip}, {active, false}, binary],
-    {ok, Sock} = gen_udp:open(State#state.port, SockOpts),
-    %% NewAddressing = get_new_addressing(State)
+active(timeout, #state{it = N, sock = Sock,
+                       base_mac = Mac0, intf_name = Intf} = State) ->
     gen_udp:send(Sock, State#state.peer_ip, State#state.port, Data = data()),
-    gen_udp:close(Sock),
-    lager:info("Send ~p~n", [Data]),
-    {next_state, passive, State#state{iterations = N - 1}, 0}.
+    Mac1 = format_mac(Mac0, N),
+    set_mac(Mac1, Intf),
+    lager:info("Sent ~p", [Data]),
+    lager:info("Changed MAC to: ~s", [Mac1]),
+    {next_state, passive, State#state{it = N + 1}, 0}.
 
-passive(timeout, #state{iterations = 0} = State) ->
+passive(timeout, #state{it = N} = State) when N > State#state.iterations ->
     lager:info("Finising in passive state"),
     {stop, normal, State};
-passive(timeout, #state{iterations = N} = State) ->
-    SockOpts = [{ip, State#state.ip}, {active, false}, binary],
-    {ok, Sock} = gen_udp:open(State#state.port, SockOpts),
+passive(timeout, #state{it = N, sock = Sock,
+                        base_mac = Mac0, intf_name = Intf} = State) ->
     {ok, {Addr, Port, Packet}} = gen_udp:recv(Sock, 100),
+    Mac1 = format_mac(Mac0, N),
+    set_mac(Mac1, Intf),
     lager:info("Received ~p from ~p:~p", [Packet, Addr, Port]),
-    {next_state, active, State#state{iterations = N - 1}}.
-
-state_name(_Event, _From, State) ->
-    {reply, ok, state_name, State}.
+    lager:info("Changed MAC to: ~p",[Mac1]),
+    {next_state, active, State#state{it = N + 1}, 0}.
 
 handle_event(_Event, StateName, State) ->
     {next_state, StateName, State}.
@@ -80,8 +88,8 @@ handle_sync_event(_Event, _From, StateName, State) ->
 handle_info(_Info, StateName, State) ->
     {next_state, StateName, State}.
 
-terminate(_Reason, _StateName, _State) ->
-    ok.
+terminate(_Reason, _StateName, #state{sock = Sock}) ->
+    ok = gen_udp:close(Sock).
 
 code_change(_OldVsn, StateName, State, _Extra) ->
     {ok, StateName, State}.
@@ -120,6 +128,10 @@ format_mac(BaseMac, It) ->
     {First, Second} = lists:split(2, ItPart1),
     BaseMac ++ First ++ ":" ++ Second.
 
+set_mac(Mac, Intf) ->
+    Cmd = io_lib:format("ip link set ~s address ~s",[Intf, Mac]),
+    [] = os:cmd(Cmd).
+
 base_ip(PairNo, InitState) when PairNo =< 16#FFFF - 1 ->
     Ip0 = case InitState of
               active ->
@@ -142,6 +154,11 @@ format_ip(BaseIp, It) ->
     {FirstOct, SecondOct} = lists:split(2, ItPart1),
     BaseIp ++ integer_to_list(list_to_integer(FirstOct, 16)) ++ "." ++
         integer_to_list(list_to_integer(SecondOct, 16)).
+
+open_socket(State) ->
+    SockOpts = [{ip, State#state.ip}, {active, false}, binary],
+    {ok, Sock} = gen_udp:open(State#state.port, SockOpts),
+    State#state{sock = Sock}.
 
 
 
